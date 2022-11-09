4134cdd2f240214ab570e4f1290a728f8cebc37b
DungTran582001 <tranmanhdung582001@gmail.com>
2022-11-09 21:32:18 +0700

    build models and templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "app/dashboard.html")
+
+def product(request):
+    return render(request, "app/product.html")
+
+def customer(request):
+    return render(request, "app/customer.html")
